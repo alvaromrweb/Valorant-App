098942bc0e9f50e029e96b6ee558806aa7da671f
alvaromrweb <alvaromrweb@gmail.com>
2023-04-17 22:00:00 +0200

    Add shadow to example text & rounded right to btn

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -14,16 +14,16 @@ export default function SearchForm({loading, setNameTag, example = null}) {
     <form className="mb-[25%]  gap-5 w-11/12 md:w-1/2 text-left">
         <div className="w-full flex">
             <div className="w-3/5 md:w-3/4">
-            <input type="text" id="search" placeholder="Buscar Nombre#Tag" className="rounded-l-lg shadow-lg bg-slate-900/75 text-white placeholder:text-gray-300 outline-none px-3 py-2 w-full" value={search} onChange={e => setSearch(e.target.value)} />
-            {example && 
-              <small className="text-gray-300 text-left cursor-pointer" onClick={e => setSearch(example)}>Por ejemplo: &quot;{example}&quot;</small>
-            }
-            
+              <input type="text" id="search" placeholder="Buscar Nombre#Tag" className="rounded-l-lg drop-shadow-lg bg-slate-900/75 text-white placeholder:text-gray-300 outline-none px-3 py-2 w-full" value={search} onChange={e => setSearch(e.target.value)} />
+              {example && 
+                <small className="text-gray-300 text-left cursor-pointer" style={{textShadow:'#000 0px 0 10px'}} onClick={e => setSearch(example)}>Por ejemplo: &quot;{example}&quot;</small>
+              }
+              
             </div>
             <div className="w-2/5 md:w-1/4">
                 <button 
                 type="submit" 
-                className="w-full text-xl bg-white hover:bg-[#ff4357] text-slate-900 hover:text-white drop-shadow-lg transition-colors py-2 px-10  font-bold rounded flex justify-center items-center gap-2 disabled:opacity-75 h-[40px]"
+                className="w-full text-xl bg-white hover:bg-[#ff4357] text-slate-900 hover:text-white drop-shadow-lg  rounded-r-lg transition-colors py-2 px-10  font-bold flex justify-center items-center gap-2 disabled:opacity-75 h-[40px]"
                 onClick={handleSearch}>
                   {loading ? 
                     <Spinner /> 
