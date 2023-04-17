c27e6695f1e1ce470e6125ddef78ab2641f4427b
alvaromrweb <alvaromrweb@gmail.com>
2023-04-17 19:22:09 +0200

    Add player card

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,15 +1,13 @@
 
-export default function PlayerCard({playerData}) {
+export default function PlayerCard({profile, mmr}) {
   return (
-    <article className="p-5 relative">
-        <img src={playerData.card.large} />
-        <div className="absolute inset-x-0 top-0">
-            <h1 className="text-3xl font-bold ">
-            {playerData.name}
-            </h1>
-            <h3>Level {playerData.account_level}</h3>
-
+    <div className="relative mt-5">
+        <img src={profile.card.large} />
+        <p className="absolute inset-x-0 top-4 text-lg ">Lvl {profile.account_level}</p>
+        <h2 className="absolute inset-x-0 bottom-[30%] text-4xl font-bold ">{profile.name}</h2>
+        <div className="absolute inset-x-0 bottom-[15%] flex flex-col justify-center items-center gap-2 text-xl">
+          <img src={mmr.image} />
         </div>
-    </article>
+    </div>
   )
 }
