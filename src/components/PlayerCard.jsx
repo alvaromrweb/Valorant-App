80be02112196d9a2261f6d85f4b7cc3273875d08
alvaromrweb <alvaromrweb@gmail.com>
2023-04-17 23:16:43 +0200

    Add tooltips to rank and progress bar

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,3 +1,5 @@
+import 'react-tooltip/dist/react-tooltip.css'
+import { Tooltip } from 'react-tooltip'
 
 export default function PlayerCard({profile, mmr}) {
   return (
@@ -9,18 +11,24 @@ export default function PlayerCard({profile, mmr}) {
           <div className="w-10/12 bg-gray-200  h-2.5 dark:bg-gray-700 relative">
             <div className="bg-[#64C2A7] h-2.5 " style={{width: `${mmr.currentPoints}%`}}></div>
             <div 
-              className="h-2.5  absolute top-0 opacity-50"
+              className="h-2.5 absolute top-0 opacity-50 pointsChange"
               style={{
                 width: `${Math.abs(mmr.pointsChangeLastGame)}%`, 
                 backgroundColor: mmr.pointsChangeLastGame > 0 ? '#00684b' : '#ff4357',
                 left: `${mmr.pointsChangeLastGame > 0 ? mmr.currentPoints - Math.abs(mmr.pointsChangeLastGame) : mmr.currentPoints}%`
               }}
             ></div>
+            <Tooltip anchorSelect=".pointsChange" place="top">
+              {mmr.pointsChangeLastGame > 0 ? `+${mmr.pointsChangeLastGame}` : mmr.pointsChangeLastGame}
+            </Tooltip>
           </div>
           {mmr.currentPoints} pts
         </div>
         <div className="absolute inset-x-0 bottom-[15%] flex flex-col justify-center items-center gap-2 text-xl">
-          <img src={mmr.image} />
+          <img className='rank' src={mmr.image} />
+          <Tooltip anchorSelect=".rank" place="bottom">
+            {mmr.rank}
+          </Tooltip>
         </div>
     </div>
   )
