55332af5455ee8891bcd19ad74a86b3f023df19d
alvaromrweb <alvaromrweb@gmail.com>
2023-04-20 00:13:38 +0200

    Add function to add ordinal to number

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -6,3 +6,9 @@ export const changeWebFavicon = favicon => {
     let link = document.querySelector("link[rel~='icon']");
     link.href = favicon;
 }
+
+export const getNumberWithOrdinal = n => {
+    var s = ["th", "st", "nd", "rd"],
+        v = n % 100;
+    return n + (s[(v - 20) % 10] || s[v] || s[0]);
+  }
