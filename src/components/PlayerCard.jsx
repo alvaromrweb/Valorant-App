acfa8061610874daf76bff966eb558ab7a493c4f
alvaromrweb <alvaromrweb@gmail.com>
2023-04-20 00:14:35 +0200

    Make the rank progress bar a separate component

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,46 +1,26 @@
 import 'react-tooltip/dist/react-tooltip.css'
 import { Tooltip } from 'react-tooltip'
+import RankProgressBar from './RankProgressBar'
 
 export default function PlayerCard({profile, mmr}) {
   return (
     <div className="relative">
         <img src={profile.card.large} />
-        <div className='absolute inset-x-0 top-[-1rem]'>
+        <div className='absolute inset-x-0 top-[-0.8rem]'>
           <span className="text-lg py-1 px-3 rounded-lg bg-slate-900/75" style={{textShadow:'#000 0px 0 10px'}}>Lvl {profile.account_level}</span>
         </div>
         <h2 className="absolute inset-x-0 bottom-[35%] text-4xl font-bold ">{profile.name}</h2>
         <div className="absolute inset-x-0 bottom-[27%] flex justify-center items-center flex-col">
-          <div className="w-10/12 bg-gray-200  h-2.5 dark:bg-gray-700 relative">
-            <div className="bg-[#64C2A7] h-2.5 " style={{width: `${mmr.currentPoints}%`}}></div>
-            <div 
-              className="h-2.5 absolute top-0 opacity-50 pointsChange"
-              style={{
-                width: `${
-                  mmr.pointsChangeLastGame > mmr.currentPoints ? 
-                  mmr.currentPoints : 
-                  Math.abs(mmr.pointsChangeLastGame)
-                }%`, 
-                backgroundColor: mmr.pointsChangeLastGame > 0 ? 'white' : '#ff4357',
-                left: `${
-                  mmr.pointsChangeLastGame > 0 ? 
-                    mmr.pointsChangeLastGame > mmr.currentPoints ? 
-                    0 :
-                    mmr.currentPoints - Math.abs(mmr.pointsChangeLastGame)
-                  : 
-                  mmr.currentPoints
-                }%`
-              }}
-            ></div>
-            <Tooltip anchorSelect=".pointsChange" place="top">
-              {mmr.pointsChangeLastGame > 0 ? `+${mmr.pointsChangeLastGame}` : mmr.pointsChangeLastGame}
-            </Tooltip>
+          <div className='w-10/12'>
+            <RankProgressBar mmr={mmr}>
+              {mmr.ranking_in_tier} pts
+            </RankProgressBar>
           </div>
-          <span style={{textShadow:'#000 0px 0 10px'}}>{mmr.currentPoints} pts</span>
         </div>
         <div className="absolute inset-x-0 bottom-[15%] flex flex-col justify-center items-center gap-2 text-xl">
-          <img className='rank' src={mmr.image} />
+          <img className='rank' src={mmr.images.small} />
           <Tooltip anchorSelect=".rank" place="bottom">
-            {mmr.rank}
+            {mmr.currenttierpatched}
           </Tooltip>
         </div>
     </div>
---
+++ src/components/RankProgressBar.jsx
@@ -0,0 +1,41 @@
+import { Tooltip } from "react-tooltip"
+
+export default function RankProgressBar({children, mmr}) {
+
+    const {ranking_in_tier, mmr_change_to_last_game, match_id} = mmr
+
+  return (
+    <div className="text-center">
+        {ranking_in_tier <= 100 &&
+            <div className="w-full bg-gray-200  h-2.5 dark:bg-gray-700 relative ">
+                <div className="bg-[#64C2A7] h-2.5 " style={{width: `${ranking_in_tier}%`}}></div>
+                <div 
+                    className={`h-2.5 absolute top-0 opacity-50 pointsChange${match_id}`}
+                    style={{
+                    width: `${
+                        mmr_change_to_last_game > ranking_in_tier ? 
+                        ranking_in_tier : 
+                        Math.abs(mmr_change_to_last_game)
+                    }%`, 
+                    backgroundColor: mmr_change_to_last_game > 0 ? 'white' : '#ff4357',
+                    left: `${
+                        mmr_change_to_last_game > 0 ? 
+                        mmr_change_to_last_game > ranking_in_tier ? 
+                        0 :
+                        ranking_in_tier - Math.abs(mmr_change_to_last_game)
+                        : 
+                        ranking_in_tier
+                    }%`
+                    }}
+                ></div>
+                <Tooltip anchorSelect={`.pointsChange${match_id}`} place="top">
+                    {mmr_change_to_last_game > 0 ? `+${mmr_change_to_last_game}` : mmr_change_to_last_game}
+                </Tooltip>
+            </div>
+        }
+        <span style={{textShadow:'#000 0px 0 10px'}}>
+            {children}
+        </span>
+    </div>
+  )
+}
