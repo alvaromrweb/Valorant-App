e869463dc916d167e426131ec5154677a8b8d031
alvaromrweb <alvaromrweb@gmail.com>
2023-04-20 00:15:01 +0200

    Add player position component for match list

---
+++ src/components/PlayerPosition.jsx
@@ -0,0 +1,25 @@
+import { getNumberWithOrdinal } from "../helpers"
+
+export default function PlayerPosition({playerPosition}) {
+    let bgColor = ''
+    switch (playerPosition) {
+        case 1:
+            bgColor = 'bg-amber-500'
+            break;
+        case 2:
+            bgColor = 'bg-slate-400'
+            break;
+        case 3:
+            bgColor = 'bg-yellow-800'
+            break;
+        default:
+            bgColor = 'bg-gray-500'
+            break;
+    }
+      
+  return (
+    <span className={`rounded-full py-1 px-5 ${bgColor}`}>
+        {playerPosition === 1 ? 'MVP' : getNumberWithOrdinal(playerPosition)}
+    </span>
+  )
+}
