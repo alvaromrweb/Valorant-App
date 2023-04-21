7abbc4f804cda0a3cfde02490727a78fbd39b832
alvaromrweb <alvaromrweb@gmail.com>
2023-04-21 12:24:36 +0200

    Fix responsive on search view

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ function App() {
   return (
     <div className="bg-slate-900 text-white">
       <div className="bg-[url('/valorant-bg.jpg')] bg-slate-900 bg-fixed bg-cover bg-center">
-        <div className="container flex flex-col justify-center items-center mx-auto text-center min-h-screen pb-5">
+        <div className="container flex flex-col md:justify-center items-center mx-auto text-center min-h-screen pb-5">
           {Object.keys(profile).length > 0 && profileMatches.length > 0 && profileMMRHistory.length > 0 ? 
             <Profile 
               profile={profile} 
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -11,25 +11,25 @@ export default function SearchForm({error, loading, setNameTag, example = null})
   }
   
   return (
-    <form className="mb-[25%]  gap-5 w-11/12 md:w-1/2 text-left">
+    <form className="mt-[15%] md:mt-0 md:mb-[25%]  gap-5 w-11/12 md:w-1/2 text-left">
         <div className="w-full flex">
-            <div className="w-3/5 md:w-3/4">
+            <div className="w-9/12 md:w-3/4">
               <input type="text" id="search" placeholder="Search Player Name#Tagline" className="rounded-l-lg drop-shadow-lg bg-slate-900/75 text-white placeholder:text-gray-300 outline-none px-3 py-2 w-full" value={search} onChange={e => setSearch(e.target.value)} />
               {example && 
                 <small className="text-gray-300 text-left cursor-pointer" style={{textShadow:'#000 0px 0 10px'}} onClick={e => setSearch(example)}>Example: &quot;{example}&quot;</small>
               }
               
             </div>
-            <div className="w-2/5 md:w-1/4">
+            <div className="w-3/12 md:w-1/4">
                 <button 
                   type="submit" 
-                  className="w-full text-xl bg-white hover:bg-[#ff4357] text-slate-900 hover:text-white drop-shadow-lg  rounded-r-lg transition-colors py-2 px-5  font-bold flex justify-center items-center gap-2 disabled:opacity-75 h-[40px]"
+                  className="w-full md:text-xl bg-white hover:bg-[#ff4357] text-slate-900 hover:text-white drop-shadow-lg  rounded-r-lg transition-colors py-2 px-5  font-bold flex justify-center items-center gap-2 disabled:opacity-75 h-[40px]"
                   onClick={handleSearch}
                   disabled={loading}
                 >
                   {loading ? 
                     <>
-                      Searching...
+                      <span className="hidden 2xl:block">Searching...</span>
                       <Spinner /> 
                     </>
                     : 'Search'
