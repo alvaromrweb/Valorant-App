f4614633b72aa169f951b98cc89b9a8e96eca2c3
alvaromrweb <alvaromrweb@gmail.com>
2023-04-25 17:02:20 +0200

    Refactor with custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,19 @@
-import { useState, useEffect } from 'react';
-import HenrikDevValorantAPI from 'unofficial-valorant-api';
+import { useState } from 'react';
 import SearchForm from './components/SearchForm';
 import Profile from './components/Profile';
 import { changeWebFavicon, changeWebTitle } from './helpers';
-
-const VAPI = new HenrikDevValorantAPI();
-
-const getAllProfileData = async nameTag => {
-  try {
-    const account = await VAPI.getAccount(nameTag)
-    const [MMRHistory, matches] = await Promise.all([
-      VAPI.getMMRHistory({...nameTag, region: account.data.region}),
-      VAPI.getMatches({...nameTag, region: account.data.region, size: 10})
-    ])
-    return {account, MMRHistory, matches}
-  } catch (error) {
-    return Promise.reject(error);
-  }
-}
+import { useProfile } from './hooks/useProfile';
 
 function App() {
   const [nameTag, setNameTag] = useState({})
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
-  const [profile, setProfile] = useState({})
-  const [profileMMRHistory, setProfileMMRHistory] = useState([])
-  const [profileMatches, setProfileMatches] = useState([])
+  const {profile, profileMMRHistory, profileMatches, loading, error, resetProfile} = useProfile({nameTag})
 
   const resetApp = () => {
     setNameTag({})
-    setProfile({})
-    setProfileMMRHistory([])
-    setProfileMatches([])
-    setError('')
+    resetProfile()
     changeWebTitle('Valorant GG EZ')
     changeWebFavicon('/favicon.png')
   }
-
-  useEffect(() => {
-    if(Object.keys(nameTag).length > 0) {
-      if(!nameTag.name || !nameTag.tag) {
-        setError('Incorrect name#tag format')
-        return
-      }
-      setLoading(true)
-      setError('')
-      getAllProfileData(nameTag)
-      .then(({account, MMRHistory, matches}) => {
-        account.status === 200 ? setProfile(account.data) : setError(account.error[0].message)
-        MMRHistory.status === 200 ? setProfileMMRHistory(MMRHistory.data) : setError(MMRHistory.error[0].message)
-        matches.status === 200 ? setProfileMatches(matches.data) : setError(matches.error[0].message)
-      })
-      .catch(err => {
-        console.log(err)
-        setError('Error trying to get account information')
-      })
-      .finally(() => {
-        setLoading(false)
-      })
-    }
-  }, [nameTag])
 
   return (
     <div className="bg-slate-900 text-white">
@@ -79,10 +33,8 @@ function App() {
                 setNameTag={setNameTag} 
                 example="Wizen#0000" 
               />
-            
           }
         </div>
-
       </div>
     </div>
   )
