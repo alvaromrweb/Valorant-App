4072d8d3a8fd1b836be638bccdd684464c9925b6
alvaromrweb <alvaromrweb@gmail.com>
2023-04-25 17:03:40 +0200

    Move the match formating to an external function and implement Match component

--- src/components/MatchList.jsx
+++ src/components/MatchList.jsx
@@ -1,90 +1,17 @@
-import PlayerPosition from "./PlayerPosition"
-import RankProgressBar from "./RankProgressBar"
-import RankImage from "./RankImage"
+import { formatMatches } from "../helpers"
+import Match from "./Match"
 
 export default function MatchList({profileMatches, MMRHistory, profileId, isWizen}) {
-    let matches = profileMatches
-
-    // Add variables to matches array that facilitate accesing the info
-    matches = matches.map(match => {
-        match.playerSelected = match.players.all_players.find(player => player.puuid === profileId) // Who is the player of the current profile
-        match.players.all_players.sort((a, b) => b.stats.score - a.stats.score)
-        match.winnerTeam = match.teams.red.has_won ? 'Red' : 'Blue' // Which team won
-        match.playerWon = match.playerSelected.team === match.winnerTeam // If the player won
-        let matchDate = new Date(match.metadata.game_start_patched)
-        matchDate.setHours(matchDate.getHours() + 2); // Change timezone to Madrid
-        match.matchHour = matchDate.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true }) // Format time
-        match.matchDate = matchDate.toLocaleString('es-ES', {day: 'numeric', month: 'numeric', year: 'numeric' }) // Format date
-        match.playerSelected.stats.kda = ((match.playerSelected.stats.kills * (match.playerSelected.stats.assists / 3)) / match.playerSelected.stats.deaths).toFixed(2) // Add KDA variable
-        match.playerPosition = match.players.all_players.findIndex(player => player.puuid === profileId) + 1 // Player position in match
-        match.mmr = MMRHistory.find(mmrItem => mmrItem.match_id === match.metadata.matchid) // Link mmrHistory match object with profileMatch
-        match.playerSelected.damagePerCredits = Math.round(match.playerSelected.damage_made / (match.playerSelected.economy.spent.overall / 1000)) // Add damage per 1000 credits data calculation
-        
-        return match
-    })
+    const matches = formatMatches({matches: profileMatches, MMRHistory, profileId})
 
   return (
     <div className="flex flex-col gap-5 bg-slate-900/90 py-5 px-4 rounded text-left">
         {matches.map(match => (
-            <article 
-                className={`w-full border-l-8 rounded py-4 px-5 flex flex-wrap justify-between items-center gap-3 md:gap-5 relative
-                ${match.playerWon ? 'bg-[#64C2A7]/25 border-green-400' : 'bg-[#ff4357]/25 border-red-400'} 
-                    ${isWizen && (match.playerWon ? "before:bg-[url('/wizencara.jpg')]" : "before:bg-[url('/wizencaragrito.jpg')]")} before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:opacity-30 before:bg-no-repeat before:bg-contain md:before-bg-auto
-                `} 
+            <Match
                 key={match.metadata.matchid}
-            >
-                <div className="z-10 basis-full md:basis-auto flex flex-row md:flex-col gap-4 md:gap-0 items-center md:items-start justify-between md:justify-normal">
-                    <p className={`font-bold md:text-xl ${match.playerWon ? 'text-green-400' : 'text-red-400'}`}>
-                        {match.playerWon ? 'Victory' : 'Defeat'}
-                    </p>
-                    <p className="text-gray-300 text-xs md:text-base">{match.metadata.mode}</p>
-                    <p className="text-gray-300 text-xs md:text-base">{match.matchHour} - {match.matchDate}</p>
-                </div>
-                
-                <div className="z-10 basis-1/4 md:basis-auto">
-                    <img className="w-12 md:w-[4.5rem] max-w-fit" src={match.playerSelected.assets.agent.small} alt={`Image of agent ${match.playerSelected.character}`} />
-                </div>
-                <div className="flex flex-col basis-1/4 md:basis-auto text-center z-10">
-                    <div className="text-gray-500 text-xl whitespace-nowrap">
-                        <span className="text-white font-bold">{match.playerSelected.stats.kills}</span> / {' '}
-                        <span className="text-white font-bold">{match.playerSelected.stats.deaths}</span> / {' '}
-                        <span className="text-white font-bold">{match.playerSelected.stats.assists}</span> 
-                    </div>
-                </div>
-                <div className="text-center z-10 basis-1/4 md:basis-auto">
-                    <PlayerPosition playerPosition={match.playerPosition} />
-                </div>
-                <div className="flex flex-col 2xl:px-5 text-center z-10 basis-1/4 md:basis-auto">
-                    <p className="text-gray-400">{match.metadata.map}</p>
-                    <p className="md:text-2xl font-bold">{match.teams.red.rounds_won} : {match.teams.blue.rounds_won}</p>
-                </div>
-                <div className="w-32 hidden md:flex flex-col gap-2 z-10">
-                    {match.mmr ? (
-                        <>
-                            <div className="flex justify-center">
-                                <RankImage mmr={match.mmr} size={'small'} />
-                            </div>
-                            <RankProgressBar mmr={match.mmr}>
-                                {match.mmr.ranking_in_tier} pts
-                            </RankProgressBar>
-                        </>
-                    ) : (
-                        <div className=""></div>
-                    )}
-                </div>
-                <div className="flex flex-col text-center z-10">
-                    <p className="text-gray-400">Score</p>
-                    <span className="font-bold md:text-xl">{match.playerSelected.stats.score}</span>
-                </div>
-                <div className="flex flex-col text-center z-10">
-                    <p className="text-gray-400">Damage</p>
-                    <span className="font-bold md:text-xl">{match.playerSelected.damage_made.toLocaleString()}</span>
-                </div>
-                <div className="flex flex-col text-center z-10">
-                    <p className="text-gray-400">Economy</p>
-                    <span className="font-bold md:text-xl">{match.playerSelected.damagePerCredits > 0 ? match.playerSelected.damagePerCredits : '0'} </span>
-                </div>
-            </article>
+                match={match}
+                isWizen={isWizen}
+            />
         ))}
     </div>
   )
