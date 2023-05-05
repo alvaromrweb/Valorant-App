18fb24ff4dc99277f16b5d4756a0e14a5df118cb
alvaromrweb <alvaromrweb@gmail.com>
2023-05-05 18:50:28 +0200

    Remove unused variable

--- src/components/MatchList.jsx
+++ src/components/MatchList.jsx
@@ -4,8 +4,8 @@ import { useContext } from "react"
 import { ProfileContext } from "../context/profile"
 
 export default function MatchList() {
-  const {profile, profileMatches, profileMMRHistory, isWizen} = useContext(ProfileContext)
-    const matches = formatMatches({matches: profileMatches, MMRHistory: profileMMRHistory, profileId: profile.puuid})
+  const {profile, profileMatches, profileMMRHistory} = useContext(ProfileContext)
+  const matches = formatMatches({matches: profileMatches, MMRHistory: profileMMRHistory, profileId: profile.puuid})
 
   return (
     <div className="flex flex-col gap-5 bg-slate-900/90 py-5 px-4 rounded text-left" data-testid="matchList">
