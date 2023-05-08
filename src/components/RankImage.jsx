bbdfda0dbb5f3566868e2d29b3084779d01e3b94
alvaromrweb <alvaromrweb@gmail.com>
2023-05-08 16:40:06 +0200

    Change RankImage to be compatible with passing numbered tier

--- src/components/RankImage.jsx
+++ src/components/RankImage.jsx
@@ -3,15 +3,16 @@ import { hasRankDivisionChanged } from "../helpers"
 import IconRankChange from "./IconRankChange"
 
 export default function RankImage({mmr, uniqueSelector = mmr.match_id, placeTooltip = 'top', size = 'full'}) {
-    const rankDivisionChanged = hasRankDivisionChanged(mmr)
+    const rankImage = `https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/${mmr.currenttier}/smallicon.png`
     let rankDivisionChange
+    const rankDivisionChanged = hasRankDivisionChanged(mmr)
     if(rankDivisionChanged) {
         rankDivisionChange = mmr.mmr_change_to_last_game > 0 ? 'ascend' : 'descend'
     }
 
   return (
     <div className="relative">
-        <img className={`rank${uniqueSelector} ${size === 'small' && 'w-10'} drop-shadow-lg`} src={mmr.images.small} alt={`Image of rank ${mmr.currenttierpatched}`} />
+        <img className={`rank${uniqueSelector} ${size === 'small' && 'w-10'} drop-shadow-lg`} src={rankImage} alt={`Image of rank ${mmr.currenttierpatched}`} />
         {rankDivisionChanged && 
             <div className="absolute bottom-0 right-0">
                 <IconRankChange change={rankDivisionChange} size={size} />
