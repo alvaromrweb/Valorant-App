dc006db3571d54c6df02b36373fc049c9c91269b
alvaromrweb <alvaromrweb@gmail.com>
2023-05-19 15:28:16 +0200

    Fix class instead of className

--- src/components/MatchSkeleton.jsx
+++ src/components/MatchSkeleton.jsx
@@ -4,43 +4,43 @@ export default function MatchSkeleton() {
   return (
     <div className='w-full min-h-[7.2rem] border border-l-8 border-slate-700 rounded py-4 px-5 flex flex-wrap justify-between items-center gap-3 md:gap-5 relative'>
         <div className="z-10 basis-full md:basis-auto 2xl:basis-40 flex flex-row md:flex-col gap-4 md:gap-0 items-center md:items-start justify-between md:justify-normal">
-            <div class="w-20 h-7 rounded-full bg-slate-700 mt-2"></div>
-            <div class="w-24 h-3.5 rounded-full bg-slate-700 mt-2"></div>
-            <div class="w-24 md:w-36 h-3.5 rounded-full bg-slate-700 mt-2"></div>
+            <div className="w-20 h-7 rounded-full bg-slate-700 mt-2"></div>
+            <div className="w-24 h-3.5 rounded-full bg-slate-700 mt-2"></div>
+            <div className="w-24 md:w-36 h-3.5 rounded-full bg-slate-700 mt-2"></div>
         </div>
         <div className="z-10 basis-1/4 md:basis-auto 2xl:basis-20">
-            <div class="w-16 h-16 rounded-full bg-slate-700 mx-auto"></div>
+            <div className="w-16 h-16 rounded-full bg-slate-700 mx-auto"></div>
         </div>
         <div className="flex flex-col basis-1/4 md:basis-auto 2xl:basis-28 text-center z-10">
             <div className="text-gray-500 text-xl whitespace-nowrap">
-                <div class="w-28 h-7 rounded-full bg-slate-700 mx-auto mt-2"></div>
+                <div className="w-28 h-7 rounded-full bg-slate-700 mx-auto mt-2"></div>
             </div>
         </div>
         <div className="text-center z-10 basis-1/4 md:basis-auto 2xl:basis-20">
-            <div class="w-16 h-7 rounded-full bg-slate-700 mx-auto mt-2"></div>
+            <div className="w-16 h-7 rounded-full bg-slate-700 mx-auto mt-2"></div>
         </div>
         <div className="flex flex-col 2xl:px-5 text-center z-10 basis-1/4 md:basis-auto 2xl:basis-32">
-            <div class="w-20 h-3.5 rounded-full bg-slate-700 mx-auto mt-2"></div>
-            <div class="w-20 md:w-24 h-3.5 md:h-6 rounded-full bg-slate-700 mx-auto mt-2"></div>
+            <div className="w-20 h-3.5 rounded-full bg-slate-700 mx-auto mt-2"></div>
+            <div className="w-20 md:w-24 h-3.5 md:h-6 rounded-full bg-slate-700 mx-auto mt-2"></div>
         </div>
         <div className="w-32 hidden md:flex flex-col 2xl:basis-32 gap-2 z-10">
             <div className="flex justify-center">
-                <div class="w-10 h-10 rounded-full bg-slate-700 mx-auto"></div>
+                <div className="w-10 h-10 rounded-full bg-slate-700 mx-auto"></div>
             </div>
-            <div class="w-32 h-2.5 rounded-full bg-slate-700 mx-auto"></div>
-            <div class="w-12 h-3 rounded-full bg-slate-700 mx-auto"></div>
+            <div className="w-32 h-2.5 rounded-full bg-slate-700 mx-auto"></div>
+            <div className="w-12 h-3 rounded-full bg-slate-700 mx-auto"></div>
         </div>
         <div className="flex flex-col 2xl:basis-14 text-center z-10">
-            <div class="w-14 h-3.5 rounded-full bg-slate-700 mx-auto mt-2"></div>
-            <div class="w-14 h-3.5 rounded-full bg-slate-700 mx-auto mt-2"></div>
+            <div className="w-14 h-3.5 rounded-full bg-slate-700 mx-auto mt-2"></div>
+            <div className="w-14 h-3.5 rounded-full bg-slate-700 mx-auto mt-2"></div>
         </div>
         <div className="flex flex-col 2xl:basis-14 text-center z-10">
-            <div class="w-14 h-3.5 rounded-full bg-slate-700 mx-auto mt-2"></div>
-            <div class="w-14 h-3.5 rounded-full bg-slate-700 mx-auto mt-2"></div>
+            <div className="w-14 h-3.5 rounded-full bg-slate-700 mx-auto mt-2"></div>
+            <div className="w-14 h-3.5 rounded-full bg-slate-700 mx-auto mt-2"></div>
         </div>
         <div className="flex flex-col 2xl:basis-14 text-center z-10">
-            <div class="w-14 h-3.5 rounded-full bg-slate-700 mx-auto mt-2"></div>
-            <div class="w-14 h-3.5 rounded-full bg-slate-700 mx-auto mt-2"></div>
+            <div className="w-14 h-3.5 rounded-full bg-slate-700 mx-auto mt-2"></div>
+            <div className="w-14 h-3.5 rounded-full bg-slate-700 mx-auto mt-2"></div>
         </div>
     </div>
   )
--- src/components/ProfileSkeleton.jsx
+++ src/components/ProfileSkeleton.jsx
@@ -11,19 +11,19 @@ export default function ProfileSkeleton() {
                         <div className='h-28  md:w-[226px] md:h-[538px] border border-white/25 rounded-md'></div>
                     </div>
                     <div className='absolute inset-x-0 md:inset-x-0 top-[-0.8rem] '>
-                        <div class="w-20 h-8 rounded-lg bg-slate-700 mx-auto"></div>
+                        <div className="w-20 h-8 rounded-lg bg-slate-700 mx-auto"></div>
                     </div>
                     <div className="absolute inset-x-0 bottom-3 md:inset-x-0 md:bottom-[35%]">
-                        <div class="w-32 h-4 rounded-full bg-slate-700  mx-auto"></div>
+                        <div className="w-32 h-4 rounded-full bg-slate-700  mx-auto"></div>
                     </div>
                     <div className="absolute right-4 inset-y-0 md:inset-y-auto md:right-0 md:inset-x-0 md:bottom-[12%] flex justify-center items-center">
                         <div className='w-10/12 flex flex-col-reverse md:flex-col items-center md:items-stretch gap-1 md:gap-0'>
                             <div className='hidden md:block text-xs md:text-base w-20 md:w-auto'>
-                                <div class="w-44 h-4 rounded-full bg-slate-700 mx-auto"></div>
-                                <div class="w-12 h-4 rounded-full bg-slate-700 mx-auto mt-2"></div>
+                                <div className="w-44 h-4 rounded-full bg-slate-700 mx-auto"></div>
+                                <div className="w-12 h-4 rounded-full bg-slate-700 mx-auto mt-2"></div>
                             </div>
                             <div className='flex justify-center md:mt-2 '>
-                                <div class="w-16 h-16 rounded-full bg-slate-700 mx-auto"></div>
+                                <div className="w-16 h-16 rounded-full bg-slate-700 mx-auto"></div>
                             </div>
                         </div>
                     </div>
