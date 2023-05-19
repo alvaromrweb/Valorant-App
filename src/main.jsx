7d2ac188cfae93d11b4aada5347bfc77d447bf1f
alvaromrweb <alvaromrweb@gmail.com>
2023-05-19 14:33:36 +0200

    Add react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ProfileProvider } from './context/profile'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ProfileProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ProfileProvider>
   </React.StrictMode>,
 )
