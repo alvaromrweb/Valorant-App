2175b3650974e353bbea2baa3625e87cb122d17d
alvaromrweb <alvaromrweb@gmail.com>
2023-05-26 13:32:38 +0200

    Change the Home element and add Leaderboard route

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import SearchForm from './components/SearchForm';
+import Home from './components/Home';
 import Profile from './components/Profile';
 import Header from './components/Header';
 import { Route, Routes } from 'react-router-dom';
+import Leaderboard from './components/Leaderboard';
 
 function App() {
 
   return (
     <div className="bg-slate-900 text-white">
       <div className="bg-[url('/valorant-bg.jpg')] bg-slate-900 bg-fixed bg-cover bg-center">
-        {/* <Header /> */}
+        <Header />
         <div className="container flex flex-col md:justify-center items-center mx-auto text-center min-h-screen pb-5">
           <Routes>
-            <Route path='/' element={<SearchForm />} />
+            <Route path='/' element={<Home />} />
             <Route path='/player/:nametag' element={<Profile />} />
+            <Route path='/leaderboard' element={<Leaderboard />} />
           </Routes>
         </div>
       </div>
