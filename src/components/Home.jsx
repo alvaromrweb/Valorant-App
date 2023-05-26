974d5523193f899040e26ce66f85e5dee3087513
alvaromrweb <alvaromrweb@gmail.com>
2023-05-26 13:31:57 +0200

    Add Home component instead of directly loading the searchForm on home

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import SearchForm from "./SearchForm"
+
+export default function Home () {
+  return (
+    <div className="mt-[15%] md:mt-0 md:mb-[25%] w-11/12 md:w-1/2">
+        <SearchForm withExample />
+    </div>
+  )
+}
