262fe9ecf6cfcce58a303c88eb6db64755b4d70d
alvaromrweb <alvaromrweb@gmail.com>
2023-06-19 14:29:35 +0200

    Change api endpoint request to request by puuid

--- src/services/profile.js
+++ src/services/profile.js
@@ -6,8 +6,8 @@ export const getAllProfileData = async nameTag => {
       const nameTagObj = {name: nametagArr[0], tag: nametagArr[1]}
       const {data: account} = await axios.get(`${import.meta.env.VITE_DATA_API_URL}/v1/account/${encodeURI(nameTagObj.name)}/${encodeURI(nameTagObj.tag)}`)
       const [{data: MMRHistory}, {data: matches}] = await Promise.all([
-        axios.get(`${import.meta.env.VITE_DATA_API_URL}/v1/mmr-history/${account.data.region}/${encodeURI(nameTagObj.name)}/${encodeURI(nameTagObj.tag)}`),
-        axios.get(`${import.meta.env.VITE_DATA_API_URL}/v3/matches/${account.data.region}/${encodeURI(nameTagObj.name)}/${encodeURI(nameTagObj.tag)}?size=10`)
+        axios.get(`${import.meta.env.VITE_DATA_API_URL}/v1/by-puuid/mmr-history/${account.data.region}/${encodeURI(account.data.puuid)}`),
+        axios.get(`${import.meta.env.VITE_DATA_API_URL}/v3/by-puuid/matches/${account.data.region}/${encodeURI(account.data.puuid)}?size=10`)
       ])
       return {account, MMRHistory, matches}
     } catch (error) {
