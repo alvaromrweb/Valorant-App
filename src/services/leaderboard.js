0dfb45f6ab93a9b1547e2e13a778f46e4a81920d
alvaromrweb <alvaromrweb@gmail.com>
2024-07-17 00:28:29 +0200

    Add API key to requests

--- src/services/leaderboard.js
+++ src/services/leaderboard.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getLeaderboard = async (region = "eu") => {
     try {
-      const leaderboard = await axios.get(`${import.meta.env.VITE_DATA_API_URL}/v2/leaderboard/${region}`)
+      const leaderboard = await axios.get(`${import.meta.env.VITE_DATA_API_URL}/v2/leaderboard/${region}${import.meta.env.VITE_DATA_API_KEY}`)
       return leaderboard
     } catch (error) {
       return Promise.reject(error);
--- src/services/profile.js
+++ src/services/profile.js
@@ -4,10 +4,10 @@ export const getAllProfileData = async nameTag => {
     try {
       const nametagArr = nameTag.split('#')
       const nameTagObj = {name: nametagArr[0], tag: nametagArr[1]}
-      const {data: account} = await axios.get(`${import.meta.env.VITE_DATA_API_URL}/v1/account/${encodeURI(nameTagObj.name)}/${encodeURI(nameTagObj.tag)}`)
+      const {data: account} = await axios.get(`${import.meta.env.VITE_DATA_API_URL}/v1/account/${encodeURI(nameTagObj.name)}/${encodeURI(nameTagObj.tag)}${import.meta.env.VITE_DATA_API_KEY}`)
       const [{data: MMRHistory}, {data: matches}] = await Promise.all([
-        axios.get(`${import.meta.env.VITE_DATA_API_URL}/v1/by-puuid/mmr-history/${account.data.region}/${encodeURI(account.data.puuid)}`),
-        axios.get(`${import.meta.env.VITE_DATA_API_URL}/v3/by-puuid/matches/${account.data.region}/${encodeURI(account.data.puuid)}?size=10`)
+        axios.get(`${import.meta.env.VITE_DATA_API_URL}/v1/by-puuid/mmr-history/${account.data.region}/${encodeURI(account.data.puuid)}${import.meta.env.VITE_DATA_API_KEY}`),
+        axios.get(`${import.meta.env.VITE_DATA_API_URL}/v3/by-puuid/matches/${account.data.region}/${encodeURI(account.data.puuid)}${import.meta.env.VITE_DATA_API_KEY}&size=10`)
       ])
       return {account, MMRHistory, matches}
     } catch (error) {
